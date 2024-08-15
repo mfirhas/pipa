aa7b2b708c36118a55daec2933e66487be0380f2
mfirhas <mfathirirhas@gmail.com>
2024-08-15 18:10:29 +0700

    Add pipa_try for Tryable types

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,40 @@
+const OPTION_TYPE: &str = "core::option::Option";
+const RESULT_TYPE: &str = "core::result::Result";
+
+enum Types {
+    Tryable,
+    Others,
+}
+
+fn type_of<T>(_: &T) -> Types {
+    let type_str = std::any::type_name::<T>();
+    if type_str.len() >= 20 {
+        match &type_str[..20] {
+            OPTION_TYPE => return Types::Tryable,
+            RESULT_TYPE => return Types::Tryable,
+            _ => return Types::Others,
+        }
+    }
+    Types::Others
+}
+
 #[macro_export]
 macro_rules! pipa {
     ($init:expr $(=> $fn:expr)*) => {{
         let r = $init;
         $(
             let r = $fn(r);
+        )*
+        r
+    }};
+}
+
+#[macro_export]
+macro_rules! pipa_try {
+    ($init:expr $(=> $fn:expr)*) => {{
+        let r = $init;
+        $(
+            let r = $fn(r)?;
         )*
         r
     }};
@@ -29,5 +60,26 @@ mod pipa_tests {
     fn pipe() {
         let ret = pipa!(123 => f => g => h);
         assert_eq!(135, ret);
+    }
+
+    // ----
+
+    fn add_one(x: i32) -> Option<i32> {
+        Some(x + 1)
+    }
+
+    fn double(x: i32) -> Option<i32> {
+        Some(x * 2)
+    }
+
+    fn sdf() -> Option<i32> {
+        let result = pipa_try!(5 => add_one => double);
+        Some(result)
+    }
+
+    #[test]
+    fn pipe2() {
+        let result = sdf().unwrap_or_default();
+        assert_eq!(12, result);
     }
 }
