b67c0e1c1259917e8f75e8d50ead59e543a33ee3
mfirhas <mfathirirhas@gmail.com>
2024-08-15 13:07:03 +0700

    Feature: chaining functions with different input and output types

---
+++ README.md
@@ -0,0 +1,8 @@
+# pipa
+
+Pipe Operator Library for Rust.
+
+Features:
+- Pipe things into chain of functions.
+- Support different types.
+- ...
--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,33 @@
+#[macro_export]
+macro_rules! pipa {
+    ($init:expr $(=> $fn:expr)*) => {{
+        let r = $init;
+        $(
+            let r = $fn(r);
+        )*
+        r
+    }};
+}
 
+#[cfg(test)]
+mod pipa_tests {
+
+    fn f(a: i32) -> u64 {
+        (a + 1) as u64
+    }
+
+    fn g(a: u64) -> String {
+        (a + 1).to_string()
+    }
+
+    fn h(s: String) -> u64 {
+        let ret = s.parse::<u64>();
+        ret.unwrap() + 10
+    }
+
+    #[test]
+    fn pipe() {
+        let ret = pipa!(123 => f => g => h);
+        assert_eq!(135, ret);
+    }
+}
