70d5459323fc6bee39b48958dab4b474e0b977dc
mfirhas <mfathirirhas@gmail.com>
2024-08-15 19:08:08 +0700

    Remove dead codes

--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,3 @@
-const OPTION_TYPE: &str = "core::option::Option";
-const RESULT_TYPE: &str = "core::result::Result";
-
-enum Types {
-    Tryable,
-    Others,
-}
-
-fn type_of<T>(_: &T) -> Types {
-    let type_str = std::any::type_name::<T>();
-    if type_str.len() >= 20 {
-        match &type_str[..20] {
-            OPTION_TYPE => return Types::Tryable,
-            RESULT_TYPE => return Types::Tryable,
-            _ => return Types::Others,
-        }
-    }
-    Types::Others
-}
-
 #[macro_export]
 macro_rules! pipa {
     ($init:expr $(=> $fn:expr)*) => {{
